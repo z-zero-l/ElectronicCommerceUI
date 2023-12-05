d4cad52a6951e8b007b32ac2ccbc7bd95f0f3fbc
1103386023 <1103386023@qq.com>
2023-12-05 17:18:18 +0800

    feat: 注册页静态结构

--- src/views/login/register.vue
+++ src/views/login/register.vue
@@ -1,8 +1,46 @@
 <script setup>
 </script>
-
 <template>
-  注册
+  <main>
+    <!-- login register wrapper start -->
+    <div class="login-register-wrapper pt-100  pt-sm-58 pb-sm-58">
+      <div class="container">
+        <div class="member-area-from-wrap">
+          <div class="row">
+            <!-- Login Content Start -->
+            <div class="col-lg-6 mx-auto my-5">
+              <div class="login-reg-form-wrap pr-lg-50 shadow-lg rounded rounded-3">
+                <h2>Sign up<a href="#" class="forget-pwd fs-6 float-end">Login<i
+                      class="bi bi-arrow-right-short"></i></a></h2>
+                <form action="#" method="post">
+                  <div class="single-input-item">
+                    <input placeholder="Enter your Account" required />
+                  </div>
+                  <div class="single-input-item">
+                    <input type="password" placeholder="Enter your Password" required />
+                  </div>
+                  <div class="single-input-item">
+                    <input type="password" placeholder="Sure your Password" required />
+                  </div>
+                  <div class="single-input-item">
+                    <div class="login-reg-form-meta d-flex align-items-center justify-content-between">
+                      <div class="remember-meta"> 
+                      </div>
+                    </div>
+                  </div>
+                  <div class="single-input-item">
+                    <button class="sqr-btn">Register</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <!-- Login Content End -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- login register wrapper end -->
+  </main>
 </template>
 
 <style scoped>
