abd4749a0e81754d9d64bf3b93914dbdb7d2a42b
1103386023 <1103386023@qq.com>
2023-12-10 18:18:30 +0800

    feat: 商家页静态结构

---
+++ src/components/product/businessItem.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  
+      <div class="col mt-3">
+        <div class="product-item mb-20" style="max-width: 90%; ">
+          <div class="product-thumb">
+            <a href="product-details.html">
+              <img src="@/assets/img/product/product-2.jpg" alt="product image">
+            </a>
+          </div>
+          <div class="product-description text-center">
+            <div class="product-name">
+              <h3 style="font-size: 14px; padding: 0;"><a href="product-details.html">Crown Backpacks</a></h3>
+            </div>
+            <div class="price-box ">
+              <span class="old-price ">$80.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+</template>
+
+<style scoped></style>
