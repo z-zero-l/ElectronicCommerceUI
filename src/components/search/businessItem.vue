7a24a3660ded82f3dfe25be72ba41556377c3c34
1103386023 <1103386023@qq.com>
2023-12-11 10:24:42 +0800

    feat:搜索页静态结构

---
+++ src/components/search/businessItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+<template>
+  <div class="myaccount-content">
+    <div style="display: flex;justify-content: space-between;">
+      <div style="display: flex; ">
+        <img class="img-thumbnail" src="@/assets/img/cart/cart-1.jpg"
+          style="border-radius: 50%;width: 80px; height: 80px;">
+        <h3 style="padding: 26px 8px 10px 10px ">王家学姐 </h3>
+        <div style="font-size: 12px; margin: auto 10px;"><span class="text-body-tertiary">6年老店</span></div>
+      </div>
+      <div style="margin-top: 25px;">
+        <a href="#" style="font-size: 14px;" class="forget-pwd float-end ">进店<i class="bi bi-arrow-right-short"></i></a>
+      </div>
+    </div>
+    <p class="saved-message">店铺介绍 <br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+      Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea dictumst. Suspendisse
+      ultrices mauris diam. Nullam sed aliquet elit. Mauris consequat nisi ut mauris efficitur lacinia.</p>
+  </div>
+</template>
+<style scoped></style>
