3d374e6fe877a5f5cbc88015e5b58fc8d78eb383
z-zero-l <z.zero.l@outlook.com>
2023-12-11 23:21:12 +0800

    feat: 地址列表数据绑定（除省市区）

--- src/views/address/address.vue
+++ src/views/address/address.vue
@@ -1,4 +1,30 @@
 <script setup>
+import service from "@/utils/request.js";
+import { ref, onMounted } from "vue";
+
+onMounted(()=>{
+  getAddressList()
+})
+
+// 获取地址列表
+const addressList = ref([])
+const getAddressList =async()=>{
+  service.get("/address/list").then((res)=>{
+    addressList.value = res.data
+    console.log(res.data)
+  })
+}
+
+// 修改赋值
+const addressForm=ref({})
+function changeAddress(item){
+  addressForm.value.id = item.id
+  addressForm.value.receiver = item.receiver
+  addressForm.value.contact = item.contact
+  // addressForm.value.id = item.id
+  addressForm.value.address = item.address
+  addressForm.value.isDefault = item.isDefault
+}
 </script>
 
 <template>
@@ -21,16 +47,18 @@
                           <th>收货人</th>
                           <th>联系方式</th>
                           <th>收货地址</th>
-                          <th></th>
+                          <th>移除</th>
+                          <th>修改</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>张三</td>
-                          <td>13323456534</td>
-                          <td>江苏省南京市栖霞区</td>
-                          <td style="width: 50px;"><a href="cart.html" class="check-btn sqr-btn" style=";padding: 2px ;width: 20px; height: 25px;"><i
-                        class="bi bi-arrow-right-short"></i></a></td>
+                        <tr v-for="item in addressList" :key="item.id">
+                          <td>{{item.receiver}}</td>
+                          <td>{{item.contact}}</td>
+                          <td>江苏省南京市栖霞区<i class="bi bi-yelp" style="color: #ff7e67;" v-if="item.isDefault==1"></i></td>
+                          <td style="width: 50px;"><span class="check-btn sqr-btn" style=";padding: 2px ;width: 20px; height: 25px;"><i class="bi bi-x"></i></span></td>
+                          <td style="width: 50px;"><span  @click="changeAddress(item)" class="check-btn sqr-btn" style=";padding: 2px ;width: 20px; height: 25px;"><i
+                        class="bi bi-arrow-right-short"></i></span></td>
                         </tr>
                       </tbody>
                     </table>
@@ -46,19 +74,22 @@
                 <h2 style="margin-top: 20px;">Form</h2>
                 <form action="#" method="post">
                   <div class="single-input-item">
-                    <input type="text" placeholder="收货人" required />
+                    <input type="text" placeholder="收货人" required v-model="addressForm.receiver"/>
                   </div>
                   <div class="single-input-item">
-                    <input type="email" placeholder="联系方式" required />
+                    <input placeholder="联系方式" required v-model="addressForm.contact"/>
                   </div>
                   <div class="single-input-item">
-                    <input type="email" placeholder="收货地址" required />
+                    <input placeholder="收货地址" required />
+                  </div>
+                  <div class="single-input-item">
+                    <input  placeholder="收货地址" required v-model="addressForm.address"/>
                   </div>
                   <div class="single-input-item">
                     <div class="login-reg-form-meta">
                       <div class="remember-meta">
                         <div class="custom-control custom-checkbox">
-                          <input type="checkbox" class="custom-control-input" id="subnewsletter">
+                          <input type="checkbox" class="custom-control-input" id="subnewsletter" :checked="addressForm.isDefault===1">
                           <label class="custom-control-label" for="subnewsletter">默认地址</label>
                         </div>
                       </div>
@@ -66,6 +97,7 @@
                   </div>
                   <div class="single-input-item">
                     <button class="sqr-btn">SAVE</button>
+                    <button type="reset" class="sqr-btn" style="float: right; color: #ff7e67;background-color: #fff;">CLEAR</button>
                   </div>
                 </form>
               </div>
