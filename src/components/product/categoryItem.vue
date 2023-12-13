494c95e8d598aaaa1d0bbfceea0bdf16da1e69ec
z-zero-l <z.zero.l@outlook.com>
2023-12-14 01:21:07 +0800

    feat: 分类页数据绑定

--- src/components/product/categoryItem.vue
+++ src/components/product/categoryItem.vue
@@ -1,28 +1,44 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+// 组件传参接收
+const props = defineProps({ categoryItem: Object });
+</script>
 
 <template>
-  
-      <div class="col mt-3">
-        <div class="product-item mb-20" style="max-width: 90%; ">
-          <div class="product-thumb">
-            <a href="product-details.html">
-              <img src="@/assets/img/product/product-2.jpg" alt="product image">
+  <div class="col mt-3">
+    <router-link :to="'/product/' + props.categoryItem.productId">
+      <div class="product-item mb-20">
+        <div class="product-thumb">
+          <div class="product-img">
+            <a href="#">
+              <img
+                :src="props.categoryItem.productCover"
+                alt="product image"
+                class="obj-fit w-75 h-75"
+              />
             </a>
           </div>
-          <div class="product-description text-center">
-            <div class="manufacturer">
-              <p><a href="product-details.html">Fashion Manufacturer</a></p>
-            </div>
-            <div class="product-name">
-              <h3 style="font-size: 14px; padding: 0;"><a href="product-details.html">Crown Backpacks</a></h3>
-            </div>
-            <div class="price-box ">
-              <span class="old-price ">$80.00</span>
-            </div>
+        </div>
+        <div class="product-description text-center">
+          <div class="manufacturer">
+            <router-link :to="'/business/' + props.categoryItem.businessId">
+              <p>
+                <a href="#">{{ props.categoryItem.business }}</a>
+              </p>
+            </router-link>
+          </div>
+          <div class="product-name">
+            <h3>
+              <a href="#">{{ props.categoryItem.productName }}</a>
+            </h3>
+          </div>
+          <div class="price-box">
+            <span class="regular-price">￥{{ props.categoryItem.price }}</span>
           </div>
         </div>
       </div>
-
+    </router-link>
+  </div>
 </template>
 
 <style scoped></style>
--- src/views/category/category.vue
+++ src/views/category/category.vue
@@ -5,7 +5,7 @@ import { ref, onMounted } from "vue";
 
 onMounted(() => {
   getCategoryFirstList();
-  // getCategorySecondList(categoryFirstList.value[0].categoryId)
+  getCategorySecondListAll();
 });
 
 // 获取一级分类列表
@@ -13,6 +13,19 @@ const categoryFirstList = ref([]);
 const getCategoryFirstList = async () => {
   service.get("/category/list/first").then((res) => {
     categoryFirstList.value = res.data.data;
+  });
+};
+
+// 获取全部二级分类信息
+const categorySecondListAll = ref([]);
+const getCategorySecondListAll = async () => {
+  service.get("/category/list").then((res) => {
+    for (let i = 0; i < res.data.data.length; i++) {
+      for (let j = 0; j < res.data.data[i].childCate.length; j++) {
+        categorySecondListAll.value.push(res.data.data[i].childCate[j]);
+      }
+    }
+    categorySecondList.value = categorySecondListAll.value;
   });
 };
 
@@ -31,8 +44,12 @@ function getCategorySecondList(parentId) {
         <div class="d-flex align-items-start">
           <div class="nav flex-column nav-pills me-3" role="tablist">
             <button
-              class="nav-link active"
-              style="width: 65px"
+              class="nav-link fs-6"
+              style="
+                width: 70px;
+                border-bottom: 1px solid #e3e3e3;
+                padding: 10px;
+              "
               data-bs-toggle="pill"
               role="tab"
               v-for="categoryFirst in categoryFirstList"
@@ -42,12 +59,14 @@ function getCategorySecondList(parentId) {
               {{ categoryFirst.cateName }}
             </button>
           </div>
-          <div class="tab-content ml-3">
+          <div class="tab-content ml-5">
             <div class="tab-pane fade show active" role="tabpanel">
               <div v-for="categorySecond in categorySecondList">
                 <h6 class="my-2">
                   <span>{{ categorySecond.cateName }}</span>
-                  <router-link to="/search">
+                  <router-link
+                    :to="'/search/category/' + categorySecond.categoryId"
+                  >
                     <a
                       href="#"
                       class="forget-pwd float-end"
@@ -62,7 +81,7 @@ function getCategorySecondList(parentId) {
                   style="border-top: 1px #808080 dashed"
                 >
                   <CategoryItem
-                    v-for="categoryItem in 4"
+                    v-for="categoryItem in categorySecond.productList"
                     :categoryItem="categoryItem"
                   ></CategoryItem>
                 </div>
@@ -81,10 +100,16 @@ function getCategorySecondList(parentId) {
   color: #ff7e67;
   border-radius: 0;
 }
+.nav .nav-link:hover {
+  color: #fff;
+  background-color: #ff7e67;
+  border-radius: 0;
+}
 
 .nav .active {
   background-color: #ff7e67;
   color: #fff;
 }
+
 
 </style>
