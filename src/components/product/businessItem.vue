77675009b6fd94cd7575d2cd8cf4be0eacc97a37
z-zero-l <z.zero.l@outlook.com>
2023-12-17 23:57:54 +0800

    feat: 店铺信息页商品跳转

--- src/components/product/businessItem.vue
+++ src/components/product/businessItem.vue
@@ -9,23 +9,26 @@ const rowItemSize = ref(4);
 <template>
   <div class="row product-carousel-one spt slick-arrow-style"
     v-for="r in (parseInt(productList.length / rowItemSize) + 1)" :key="r">
-    <div class="col-3 mt-3" v-for="product in props.productList.slice(4 * (r - 1), 4 * (r - 1) + 4)" :key="product.productId">
-      <div class="product-item mb-20" style="max-width: 90%; ">
-        <div class="product-thumb">
-          <a href="product-details.html">
-            <img :src="product.productCover" alt="product image">
-          </a>
-        </div>
-        <div class="product-description text-center">
-          <div class="product-name">
-            <h3 style="font-size: 14px; padding: 0;"><a href="product-details.html">{{product.productName}}</a></h3>
+    <div class="col-3 mt-3" v-for="product in props.productList.slice(4 * (r - 1), 4 * (r - 1) + 4)"
+      :key="product.productId">
+      <router-link :to="'/product/' + product.productId">
+        <div class="product-item mb-20" style="max-width: 90%; ">
+          <div class="product-thumb">
+            <a href="product-details.html">
+              <img :src="product.productCover" alt="product image">
+            </a>
           </div>
-          <div class="price-box ">
-            <span class="old-price ">￥{{ product.price }}</span>
-            <span class="old-price">{{ product.totalSaleAmount }} 人已付款</span>
+          <div class="product-description text-center">
+            <div class="product-name">
+              <h3 style="font-size: 14px; padding: 0;"><a href="product-details.html">{{ product.productName }}</a></h3>
+            </div>
+            <div class="price-box ">
+              <span class="old-price ">￥{{ product.price }}</span>
+              <span class="old-price">{{ product.totalSaleAmount }} 人已付款</span>
+            </div>
           </div>
         </div>
-      </div>
+      </router-link>
     </div>
   </div>
 </template>
